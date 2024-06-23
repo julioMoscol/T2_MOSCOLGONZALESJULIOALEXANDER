39b16c43245ff448ba924012b9d088a04aac42ac
julioMoscol <juliomoscolgonzales@gmail.com>
2024-06-22 21:36:23 -0500

    7mo commit

--- src/app/dashboard/almacen2/almacen2.component.ts
+++ src/app/dashboard/almacen2/almacen2.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../angular-material/material/material.module';
 
 @Component({
   selector: 'app-almacen2',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './almacen2.component.html',
   styleUrl: './almacen2.component.css'
 })
 export class Almacen2Component {
+  cantidad: number = 0;
+  precio: number = 0;
+  totalAPagar: number = 0;
+  descuentoAplicado: boolean = false;
 
+  calcularTotal(): void {
+    this.totalAPagar = this.cantidad * this.precio;
+
+    if (this.totalAPagar > 200) {
+      this.totalAPagar *= 0.8;
+      this.descuentoAplicado = true;
+    } else {
+      this.descuentoAplicado = false;
+    }
+  }
 }
