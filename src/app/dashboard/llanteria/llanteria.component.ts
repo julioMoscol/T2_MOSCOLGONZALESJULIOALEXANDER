1f8a4d261c4dd46697e0b4ed8b45e4664aec1462
julioMoscol <juliomoscolgonzales@gmail.com>
2024-06-22 21:29:30 -0500

    5to commit

--- src/app/dashboard/llanteria/llanteria.component.ts
+++ src/app/dashboard/llanteria/llanteria.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../angular-material/material/material.module';
 
 @Component({
   selector: 'app-llanteria',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './llanteria.component.html',
   styleUrl: './llanteria.component.css'
 })
 export class LlanteriaComponent {
+  cantidadLlantas: number = 0;
+  precioPorLlantaMenosDe5: number = 800;
+  precioPorLlanta5OMas: number = 700;
 
+  calcularTotal(): number {
+    if (this.cantidadLlantas < 5) {
+      return this.cantidadLlantas * this.precioPorLlantaMenosDe5;
+    } else {
+      return this.cantidadLlantas * this.precioPorLlanta5OMas;
+    }
+  }
 }
